4796ea162d309ca0170be357e2a1bed542f7867f
ScottWorks <scottworks.io@gmail.com>
2019-02-05 17:51:02 -0700

    added seed and getUser files

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,13 @@
-CREATE TABLE user (
-  id SERIAL PRIMARY KEY NOT NULL,
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS imageLikes;
+DROP TABLE IF EXISTS commentLikes;
+DROP TABLE IF EXISTS imageComments;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users 
+(
+  id SERIAL PRIMARY KEY,
   profileImageUrl TEXT NOT NULL,
   username TEXT NOT NULL,
   email TEXT NOT NULL,
@@ -7,39 +15,260 @@ CREATE TABLE user (
   salt TEXT NOT NULL,
   firstName TEXT NOT NULL,
   lastName TEXT NOT NULL
-)
+);
 
-CREATE TABLE following (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INTEGER REFERENCES user(id)
-)  
+CREATE TABLE followers 
+(
+  id SERIAL PRIMARY KEY,
+  userId INTEGER REFERENCES users(id),
+  followerId INTEGER
+);
 
-CREATE TABLE follower (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INTEGER REFERENCES user(id)
-)  
-
-CREATE TABLE image (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INTEGER REFERENCES user(id),
+CREATE TABLE images 
+(
+  id SERIAL PRIMARY KEY,
+  userId INTEGER REFERENCES users(id),
   url TEXT
-)  
+);  
 
-CREATE TABLE imageLike (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INTEGER REFERENCES user(id),
-  imageId INTEGER REFERENCES image(id)
-)  
+CREATE TABLE imageLikes 
+(
+  id SERIAL PRIMARY KEY,
+  userId INTEGER REFERENCES users(id),
+  imageId INTEGER REFERENCES images(id)
+);  
 
-CREATE TABLE imageComment (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INTEGER REFERENCES user(id),
-  imageId INTEGER REFERENCES image(id)
+CREATE TABLE imageComments 
+(
+  id SERIAL PRIMARY KEY,
+  userId INTEGER REFERENCES users(id),
+  imageId INTEGER REFERENCES images(id),
   comment TEXT
-)  
+);  
 
-CREATE TABLE commentLike (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INTEGER REFERENCES user(id),
-  imageCommentId INTEGER REFERENCES imageComment(id)
-)  
+CREATE TABLE commentLikes 
+(
+  id SERIAL PRIMARY KEY,
+  userId INTEGER REFERENCES users(id),
+  imageCommentId INTEGER REFERENCES imageComments(id)
+); 
+
+
+
+-- /////////// GENERATE FAKE USERS \\\\\\\\\\\\\
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/men/70.jpg', 'silverzebra254', 'ryan.young@example.com', 'bigballs', 'jreLhvWR', 'ryan', 'young');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/79.jpg', 'angrybird406', 'tracy.hopkins@example.com', 'oooooooo', 'jLLDejK5', 'tracy', 'hopkins');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/57.jpg', 'greensnake791', 'katiane.daluz@example.com', 'wordup', 'HQvP0Hjy', 'katiane', 'da luz');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/41.jpg', 'brownkoala859', 'édith.gaillard@example.com', 'rated', 'ztHr5Pha', 'édith', 'gaillard');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/82.jpg', 'lazylion643', 'sofia.rhodes@example.com', 'balloons', 'yFE88aDk', 'sofia', 'rhodes');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/11.jpg', 'blackmouse786', 'monica.williamson@example.com', 'coldplay', 'ZTilpGLy', 'monica', 'williamson');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/men/38.jpg', 'orangepanda867', 'lauri.jarvi@example.com', 'midnight', 'rqM7Kcb1', 'lauri', 'jarvi');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/63.jpg', 'greenzebra553', 'deborah.nguyen@example.com', 'lololo', 'feoznowM', 'deborah', 'nguyen');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/women/75.jpg', 'organicelephant966', 'jennifer.bryant@example.com', 'whatever', 'rZHcNd49', 'jennifer', 'bryant');
+
+-- INSERT INTO users
+--  (profileImageUrl, username, email, password, salt, firstName, lastName)
+-- VALUES
+--  ('https://randomuser.me/api/portraits/men/51.jpg', 'whitewolf702', 'eino.raisanen@example.com', 'fruit', 'azZOzJAY', 'eino', 'raisanen');
+
+
+
+--  -- /////////// GENERATE FOLLOWERS \\\\\\\\\\\\\
+
+-- -- /////////// USER 1 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 1), (SELECT id FROM users WHERE id = 2));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 1), (SELECT id FROM users WHERE id = 3));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 1), (SELECT id FROM users WHERE id = 4));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 1), (SELECT id FROM users WHERE id = 5));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 1), (SELECT id FROM users WHERE id = 6));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 1), (SELECT id FROM users WHERE id = 7));
+
+-- -- /////////// USER 2 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 2), (SELECT id FROM users WHERE id = 1));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 2), (SELECT id FROM users WHERE id = 3));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 2), (SELECT id FROM users WHERE id = 8));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 2), (SELECT id FROM users WHERE id = 9));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 2), (SELECT id FROM users WHERE id = 10));
+
+-- -- /////////// USER 3 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 3), (SELECT id FROM users WHERE id = 1));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 3), (SELECT id FROM users WHERE id = 4));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 3), (SELECT id FROM users WHERE id = 5));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 3), (SELECT id FROM users WHERE id = 10));
+
+-- -- /////////// USER 4 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 4), (SELECT id FROM users WHERE id = 2));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 4), (SELECT id FROM users WHERE id = 3));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 4), (SELECT id FROM users WHERE id = 5));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 4), (SELECT id FROM users WHERE id = 7));
+
+-- -- /////////// USER 5 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 5), (SELECT id FROM users WHERE id = 1));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 5), (SELECT id FROM users WHERE id = 2));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 5), (SELECT id FROM users WHERE id = 3));
+
+-- -- /////////// USER 6 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 6), (SELECT id FROM users WHERE id = 5));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 6), (SELECT id FROM users WHERE id = 7));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 6), (SELECT id FROM users WHERE id = 8));
+
+-- -- /////////// USER 7 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 7), (SELECT id FROM users WHERE id = 4));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 7), (SELECT id FROM users WHERE id = 5));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 7), (SELECT id FROM users WHERE id = 8));
+
+-- -- /////////// USER 8 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 8), (SELECT id FROM users WHERE id = 6));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 8), (SELECT id FROM users WHERE id = 7));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 8), (SELECT id FROM users WHERE id = 10));
+
+-- -- /////////// USER 9 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 9), (SELECT id FROM users WHERE id = 4));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 9), (SELECT id FROM users WHERE id = 5));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 9), (SELECT id FROM users WHERE id = 10));
+
+-- -- /////////// USER 10 \\\\\\\\\\\\\
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 10), (SELECT id FROM users WHERE id = 3));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 10), (SELECT id FROM users WHERE id = 7));
+
+-- INSERT INTO followers
+--  (userId, followerId)
+-- VALUES ((SELECT id FROM users WHERE id = 10), (SELECT id FROM users WHERE id = 9));
+
+
+
+-- ///////////  \\\\\\\\\\\\\
