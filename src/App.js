a7c5fbdc300cfcf02b8d7f96b6f7f3c36667e364
ScottWorks <scottworks.io@gmail.com>
2019-02-11 17:47:06 -0700

    removed state from App component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import ProfileListContainer from './containers/ProfileListContainer';
 import ProfileContainer from './containers/ProfileContainer';
 import Error404 from './components/Error404';
 import { Switch, Route } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      users: []
-    };
-  }
-
-  componentDidMount() {
-    axios.get('/api/users').then((response) => {
-      const { data } = response;
-      this.setState({ users: data });
-    });
-  }
-
-  render() {
-    const { users } = this.state;
-
-    return (
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => <ProfileListContainer users={users} />}
-          />
-          <Route path={'/users/:username'} component={ProfileContainer} />
-          <Route component={Error404} />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={ProfileListContainer} />
+        <Route path={'/users/:username'} component={ProfileContainer} />
+        <Route component={Error404} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/containers/ProfileListContainer.js
+++ src/containers/ProfileListContainer.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import ProfileList from '../views/ProfileList';
+import axios from 'axios';
 
 class ProfileListContainer extends Component {
   constructor() {
     super();
+
+    this.state = {
+      users: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/users').then((response) => {
+      const { data } = response;
+      this.setState({ users: data });
+    });
   }
 
   render() {
-    return <ProfileList {...this.props} />;
+    const { users } = this.state;
+
+    return users ? <ProfileList {...this.state} /> : null;
   }
 }
 
